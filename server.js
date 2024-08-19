b767ead448f905478f046e7c67e57daa37971d9c
Ramen96 <eric.worsfold@gmail.com>
2024-08-19 18:28:25 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express", 4.19);
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.static("files"));
+
+app.get("/", (req, res) => {
+  res.send("public");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
