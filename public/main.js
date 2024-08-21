f16e48e424e1ac2772a013e94f3d9ae9dbd2b0d1
Ramen96 <eric.worsfold@gmail.com>
2024-08-20 20:50:35 -0400

    created reset function

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
+// Determines player trun true = p1 false = p2
+let playerTurn = true;
+
+// Change box condition based on player status
+function changeXorO(boolvalue, color, id) {
+  document.getElementById(id).style.background = color;
+  playerTurn = boolvalue;
+}
+
 // Grabs button element id via param passed in onClick
 function handleClick(id) {
   const stringifiedNum = id.toString();
-  console.log(stringifiedNum);
-  // Proof of concept here
-  document.getElementById(stringifiedNum).style.background = "blue";
+  if (playerTurn) {
+    changeXorO(false, "blue", stringifiedNum);
+  } else {
+    changeXorO(true, "red", stringifiedNum);
+  }
+}
+
+// Resets boxes to original state
+function resetBoxes() {
+  const id = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  for (let i = 0; i < id.length; i++) {
+    document.getElementById(id[i]).style.background = "transparent";
+  }
 }
