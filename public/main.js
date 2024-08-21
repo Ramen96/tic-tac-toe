0d4bb63e6282c03aa4969f2c3cf361d407dbaa77
Ramen96 <eric.worsfold@gmail.com>
2024-08-20 21:45:45 -0400

    added arrays to track if players have a win condition

--- public/main.js
+++ public/main.js
@@ -1,6 +1,14 @@
 // Determines player trun true = p1 false = p2
 let playerTurn = true;
 
+// assinge to bool later to determine which player wins
+// p1 = true p2 = flase
+let win = undefined;
+
+// arrays for each win condition
+let p1Arr = [];
+let p2Arr = [];
+
 // Change box condition based on player status
 function changeXorO(boolvalue, color, id) {
   document.getElementById(id).style.background = color;
@@ -9,14 +17,20 @@ function changeXorO(boolvalue, color, id) {
 
 // Grabs button element id via param passed in onClick
 function handleClick(id) {
-  const stringifiedNum = id.toString();
   const currnetColor = document.getElementById(id).style.background;
+
   // Check if card has already been clicked
   if (currnetColor !== "red" && currnetColor !== "blue") {
     if (playerTurn) {
-      changeXorO(false, "blue", stringifiedNum);
+      changeXorO(false, "blue", id);
+      p1Arr.push(id);
+
+      // Sorting arrays to match win conditons
+      p1Arr.sort();
     } else {
-      changeXorO(true, "red", stringifiedNum);
+      changeXorO(true, "red", id);
+      p2Arr.push(id);
+      p2Arr.sort();
     }
   } else {
     console.log("This card is already in use");
@@ -30,3 +44,17 @@ function resetBoxes() {
     document.getElementById(id[i]).style.background = "transparent";
   }
 }
+
+// Win Conditions displayed as array matrix
+// Read from left to right on each line
+// 1 2 3
+// 4 5 6
+// 7 8 9
+//
+// 1 4 7
+// 2 5 8
+// 3 6 9
+//
+// 1 5 9
+// 3 5 7
+//
