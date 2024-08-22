18b1d274b3b0c178ef26bf16b139c4953df642b8
Ramen96 <eric.worsfold@gmail.com>
2024-08-21 22:19:57 -0400

    created function to check if win conditions are met

--- public/main.js
+++ public/main.js
@@ -9,6 +9,35 @@ let win = undefined;
 let p1Arr = [];
 let p2Arr = [];
 
+const winConditions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+// compairs arrays to see if win condition is met
+// need to somehow filter html elements to create arrays of elements
+// that have been clicked on, sort those arrays and feed them to this
+// function.
+function checkWinCon(arr) {
+  let x = JSON.stringify(arr);
+  for (let i = 0; i < winConditions.length; i++) {
+    let y = JSON.stringify(winConditions[i]);
+    if (x === y) {
+      console.log("Win condition met", x);
+    } else {
+      console.log("no win condition met");
+    }
+  }
+}
+
 // Change box condition based on player status
 function changeXorO(boolvalue, color, id) {
   document.getElementById(id).style.background = color;
@@ -27,10 +56,13 @@ function handleClick(id) {
 
       // Sorting arrays to match win conditons
       p1Arr.sort();
+      // alert(p1Arr);
+      checkWinCon(p1Arr);
     } else {
       changeXorO(true, "red", id);
       p2Arr.push(id);
       p2Arr.sort();
+      checkWinCon(p2Arr);
     }
   } else {
     console.log("This card is already in use");
@@ -39,22 +71,10 @@ function handleClick(id) {
 
 // Resets boxes to original state
 function resetBoxes() {
+  p1Arr = [];
+  p2Arr = [];
   const id = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   for (let i = 0; i < id.length; i++) {
     document.getElementById(id[i]).style.background = "transparent";
   }
 }
-
-// Win Conditions displayed as array matrix
-// Read from left to right on each line
-// 1 2 3
-// 4 5 6
-// 7 8 9
-//
-// 1 4 7
-// 2 5 8
-// 3 6 9
-//
-// 1 5 9
-// 3 5 7
-//
