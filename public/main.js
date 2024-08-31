3b600032edc8185e1efa89f371d0dd031345b900
Ramen96 <eric.worsfold@gmail.com>
2024-08-31 15:27:08 -0400

    game logic is done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 // Determines player trun true = p1 false = p2
-let playerTurn = true;
-
+let playerTurn = false;
 let boardState = [null, null, null, null, null, null, null, null, null];
+let gameStateWon = false;
+let player1Count = 0;
+let player2Count = 0;
 
 const winConditions = [
   [0, 1, 2],
@@ -18,12 +20,46 @@ function checkWinCon() {
   let checkX = [];
   let checkO = [];
 
+  function updateText(id) {
+    const element = document.getElementById(id);
+    if (playerTurn) {
+      if (element !== null) {
+        player1Count++;
+        element.textContent = player1Count;
+      } else {
+        console.error(`Element with ${id} not found.`);
+      }
+    } else {
+      if (element !== null) {
+        player2Count++;
+        element.textContent = player2Count;
+      } else {
+        console.error(`Element with ${id} not found.`);
+      }
+    }
+  }
+
+  function win() {
+    if (playerTurn) {
+      // alert("player 1 wins");
+      updateText("player1Score");
+      setTimeout(() => resetBoxes(), 1000);
+    }
+    if (!playerTurn) {
+      // alert("player 2 wins");
+      //
+      updateText("player2Score");
+      setTimeout(() => resetBoxes(), 1000);
+    }
+  }
+
   function compairArr(arr) {
     for (let i = 0; i < winConditions.length; i++) {
       let currnentWinCon = JSON.stringify(winConditions[i]);
       const playerArr = JSON.stringify(arr);
       if (currnentWinCon === playerArr) {
-        console.log("win condition met");
+        gameStateWon = true;
+        win();
       }
     }
   }
@@ -73,23 +109,28 @@ function changeXorO(boolvalue, color, id) {
 
 // Grabs button element id via param passed in onClick
 function handleClick(id) {
-  if (boardState[id] === null) {
-    if (playerTurn) {
-      boardState[id] = "X";
-      changeXorO(false, "blue", id);
-      checkWinCon();
+  if (gameStateWon === false) {
+    if (boardState[id] === null) {
+      if (playerTurn) {
+        boardState[id] = "X";
+        changeXorO(false, "blue", id);
+        checkWinCon();
+      } else {
+        boardState[id] = "O";
+        changeXorO(true, "red", id);
+        checkWinCon();
+      }
     } else {
-      boardState[id] = "O";
-      changeXorO(true, "red", id);
-      checkWinCon();
+      console.log("this card is already in use");
     }
   } else {
-    console.log("this card is already in use");
+    console.log("heho heho");
   }
 }
 
 // Resets boxes to original state
 function resetBoxes() {
+  gameStateWon = false;
   boardState = [null, null, null, null, null, null, null, null, null];
   const id = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
   for (let i = 0; i < id.length; i++) {
