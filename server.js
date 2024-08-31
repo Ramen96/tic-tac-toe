191bce7599616b4411437a0dceb2f7921198c9d7
Ramen96 <eric.worsfold@gmail.com>
2024-08-31 11:04:05 -0400

    set up browser sync for live reload

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express", 4.19);
+const browserSync = require("browser-sync");
+// import browserSync from "browser-sync";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.static("files"));
+
+browserSync.init({
+  proxy: "http://localhost:3000",
+  port: 3001,
+  files: ["public/**/*.*"],
+});
 
 app.get("/", (req, res) => {
   res.send("public");
